20f1ad7f8698977632a8d50e29899e54f1400d0a
Ryan Frantz <ryanleefrantz@gmail.com>
2012-10-19 16:29:11 -0400

    Reverted back to using time() as I'm about to start sending this output to Graphite.

--- conntrackReport.pl
+++ conntrackReport.pl
@@ -30,22 +30,7 @@ open( CONNTRACK, $file ) or die "Unable to open \'$file\' for reading: $!\n";
 my @lines = <CONNTRACK>;
 close CONNTRACK;
 
-sub getTimestamp {
-	# I don't have a fancy module like DateTime on this system nor the ability to install it
-	my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime(time);
-	$year += 1900;
-	$sec = '0' . $sec if $sec < 10;
-	$min = '0' . $min if $min < 10;
-	$hour = '0' . $hour if $hour < 10;	
-	$mon += 1;	# add one to the zero-indexed month
-	$mon = '0' . $mon if $mon < 10;	
-	$mday = '0' . $mday if $mday < 10;	# paaaaiiiin...
-
-	return "$year-$mon-$mday $hour:$min:$sec";
-}
-
-my $timestamp = getTimestamp();
-print $timestamp . $FS;
+print time() . $FS;
 my $connCount = @lines;	# total connections
 foreach my $connectionType ( @tcpConnectionTypes ) {
 	my $count = grep(/$connectionType/, @lines);
